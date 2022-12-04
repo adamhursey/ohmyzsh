f5c5fbc3968dea03ffb55ea98190d85c1ae0c303
Adam Hursey <adam@Adams-MacBook-Pro.local>
2022-12-04 00:08:34 -0500

    added more git aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -5,3 +5,5 @@ alias reload='source ~/.zshrc;'
 # Git alias
 alias gst='git status;'
 alias dev='cd ~/Development'
+alias gpub='git push --set-upstream origin master;'
+alias gac='git add .; git commit -m'
